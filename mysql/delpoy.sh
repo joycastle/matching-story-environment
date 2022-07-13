408a83689ddb78e522803428ebeafc3f351f7268
LevinLFX <levin@joycastle.mobi>
2022-07-13 07:23:19 -0400

    done

--- mysql/delpoy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-. ./config
-
-mkdir -p $path/conf && cp my.cnf $path/conf
-mkdir -p $path/initdb && cp initdb.sql $path/initdb
-
-../cmd/format -tpl=docker-compose-tpl.yaml -data=config -output=docker-compose.yaml
-
-docker-compose -f docker-compose.yaml up -d --force-recreate
--- redis/delpoy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-. ./config
-
-mkdir -p $path/conf && cp redis.conf $path/conf
-
-../cmd/format -tpl=docker-compose-tpl.yaml -data=config -output=docker-compose.yaml
-
-docker-compose -f docker-compose.yaml up -d --force-recreate
