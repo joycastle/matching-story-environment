39cee939b22d38464b8a166426082321f69bba24
LevinLFX <levin@joycastle.mobi>
2022-07-13 08:13:45 -0400

    done

---
+++ qalinux/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#blue
+#red
+#green
+#yellow
+#pink
+
+
+jobs_mysql_path=../jobs/mysql
+jobs_redis_path=../jobs/redis
+jobs_pigeon_path=../jobs/match-3-pigeon
+jobs_pangu_path=../jobs/match-3-pangu
+jobs_robot_path=../jobs/matching-story-robot-service
+
+
+echo \
+"color=blue
+ip=10.10.10.111
+port=13306
+path=/home/levin/code/matching-story-environment/mysql/test222222"
+
+
+echo $jobs_mysql_path/config
